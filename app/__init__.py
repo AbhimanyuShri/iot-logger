9dca077619f4307b08fcb20158c1d4da0d2e50a0
WATERMELONPOP <abhishridhar@icloud.com>
2022-06-15 15:09:08 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ def updateinfo():
         return flask.jsonify(
                 message="good test",
                 category="success",
-                data=data,
                 status=200
             )
 
