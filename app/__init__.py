650227a5f6f36067a19637e73488e543424d8847
aaaa4aaaa <xxtobynxx@gmail.com>
2022-06-16 17:17:52 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,15 @@ from authlib.flask.client import OAuth
 
 app = flask.Flask(__name__)
 
+host="ec2-107-22-238-112.compute-1.amazonaws.com"
+database="dimfu044kiidh"
+user="wttxpbzwwyiezo"
+port="5432"
+password="da62a0b0a27d53b8c813a6eb60df65b636c162a4df0336c00409558a5c3f2c14"
+uri="postgres://wttxpbzwwyiezo:da62a0b0a27d53b8c813a6eb60df65b636c162a4df0336c00409558a5c3f2c14@ec2-107-22-238-112.compute-1.amazonaws.com:5432/dimfu044kiidh"
+heroku_cli="heroku pg:psql postgresql-metric-77907 --app rpi-iot-logger"
+
+
 
 @app.route("/update",methods=["POST"])
 def updateinfo():
